bee2e0f20967ba8657da2eee3ca0a7720a268d3e
Izhar Ramadhan <izharindonesia@gmail.com>
2024-10-22 07:22:50 +0700

    php-html

--- php-html/index.php
+++ php-html/index.php
@@ -7,9 +7,10 @@
 </head>
 <body>
   <h1>Ini Halaman Login</h1>
-  <form action="./dashboard.php" method="POST"></form>
+  <form action="./dashboard.php" method="POST">
     <input type="email" name="email" id="email" placeholder="Email"/>
     <input type="text" name="password" id="password" placeholder="Password"/>
     <button type="submit">Login</button>
+  </form>
 </body>
 </html>
